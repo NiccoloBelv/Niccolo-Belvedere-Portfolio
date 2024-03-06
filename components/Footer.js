4e0906885baa1f38504f412b4b5219269da3cf08
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-06 19:37:27 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/mylogo.png" alt="Niccolò Belvedere Logo" className={styles.logo} />
       </footer>
     </>
   )
