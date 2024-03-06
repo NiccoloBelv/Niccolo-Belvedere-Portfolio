cab8ec8fde42ff7edc452c4c57efbcf4782c7d63
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-06 18:58:43 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hello, I am Niccolò Belvedere. Welcome to my personal portfolio!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
