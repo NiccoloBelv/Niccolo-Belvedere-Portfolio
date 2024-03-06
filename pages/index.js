f7068a04fb0665a55a522c2f76a95fc08fb3ee49
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-06 20:32:15 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <main>
+        <Header title="Hello, I am Niccolò Belvedere. Welcome to my personal portfolio!" />
+
         <section>
-          <h2>About Me</h2>
-          <p>
+          <h2 style={{ textAlign: 'center' }}>About Me</h2>
+          <p style={{ textAlign: 'center' }}>
             As a passionate student with a proven track record in Business Development, I specialize in Tech Sales. 
             My innovative solutions have driven measurable results in my previous experiences, demonstrating a deep understanding of customers and markets. 
             Committed to continuous learning, I stay ahead in the dynamic tech landscape.
@@ -21,7 +23,7 @@ export default function Home() {
         </section>
 
         <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
-          <section>
+          <section style={{ textAlign: 'center' }}>
             <h2>My Photo</h2>
             <img
               src="/Picture.png"
@@ -35,7 +37,7 @@ export default function Home() {
             />
           </section>
 
-          <section>
+          <section style={{ textAlign: 'center' }}>
             <h2>Download my CV</h2>
             <p>
               <a href="/CV.pdf" download>
@@ -44,7 +46,7 @@ export default function Home() {
             </p>
           </section>
 
-          <section>
+          <section style={{ textAlign: 'center' }}>
             <h2>Projects</h2>
             {/* Aggiungi qui i componenti del tuo progetto */}
           </section>
