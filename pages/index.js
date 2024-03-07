2527629f1dc5a28d996f74a759e666cf4846e469
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-07 16:47:51 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
-import styles from '@styles/Home.module.css'; // Assuming you have a CSS module for styling
+import styles from '@styles/Home.module.css';
 
 export default function Home() {
   return (
