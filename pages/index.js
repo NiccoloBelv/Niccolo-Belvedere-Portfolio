5dd3c333ad26a71ce58f2a0349a2935473dd6907
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-13 23:55:43 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -312,18 +312,18 @@ export default function Home() {
             <h2>Certificates</h2>
             <div className={styles.certificatesContainer}>
               <p>
-                <strong><a href="BCG Introduction to Strategy Consulting Virtual Experience Program.pdf" target="_blank" download>Boston Consulting Group – Strategy Consulting Virtual Experience Program on Forage</a> (February 2023)</strong><br />
+                <strong><a href="BCG Introduction to Strategy Consulting Virtual Experience Program.pdf" target="_blank" download>Boston Consulting Group – Strategy Consulting Virtual Experience Program on Forage</a> <br />(February 2023)</strong><br />
                 <em>Conducted market research, brainstorming session and adopting solutions with creative methods. </em><br />
                 <br />
-                <strong><a href="Competitive Intelligence.pdf" target="_blank" download>Competitive Intelligence Certificate on Voice of the Business</a> (April 2022)</strong><br />
+                <strong><a href="Competitive Intelligence.pdf" target="_blank" download>Competitive Intelligence Certificate on Voice of the Business</a> <br />(April 2022)</strong><br />
                 <em>Research Gathering Methods, CXM, building of KPIs, competitor analysis.</em><br />
                 <br />
-                <strong><a href="Certificate Bitcoin and Blockchain Fundamentals.pdf" target="_blank" download>Bitcoin and Blockchain Fundamentals</a> (March 2021)</strong> <br />
+                <strong><a href="Certificate Bitcoin and Blockchain Fundamentals.pdf" target="_blank" download>Bitcoin and Blockchain Fundamentals</a> <br />(March 2021)</strong> <br />
                 <br />
-                <strong><a href="ECDL and ICDL.pdf" target="_blank" download>ECDL & ICDL Full Standard (Use of Excel, PowerPoint, Word and Data Privacy)</a> (June 2020)</strong><br />
+                <strong><a href="ECDL and ICDL.pdf" target="_blank" download>ECDL & ICDL Full Standard (Use of Excel, PowerPoint, Word and Data Privacy)</a> <br />(June 2020)</strong><br />
                 <em>The certification consists of 7 topics that are divided into the following modules: Computer Essentials, Online Essentials, Word Processing, Spreadsheets, IT-Security & Cyber Security, Presentation, Online Collaboration.</em><br />
                 <br />
-                <strong><a href="Certificate Acrobat Professional.pdf" target="_blank" download>Document Management with Office and Acrobat Professional</a> (December 2019)</strong><br />
+                <strong><a href="Certificate Acrobat Professional.pdf" target="_blank" download>Document Management with Office and Acrobat Professional</a> <br />(December 2019)</strong><br />
               </p>
             </div>
           </section>
