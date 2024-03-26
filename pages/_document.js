58675cb63ef1f5e6870691327ab0272bc85f2946
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-26 09:06:38 +0100

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Puoi aggiungere tag personalizzati qui come il tuo favicon */}
+          <link rel="icon" href="favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
