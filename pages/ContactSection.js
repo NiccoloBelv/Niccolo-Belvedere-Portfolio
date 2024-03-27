223cd9484b6a7aad5e93f27bc4886cec857dad19
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-27 19:22:27 +0100

    Update ContactSection.js

--- pages/ContactSection.js
+++ pages/ContactSection.js
@@ -3,7 +3,29 @@ import React from 'react';
 import { FaEnvelope, FaPhone, FaMapMarkerAlt, FaPaperPlane } from 'react-icons/fa';
 
 const ContactSection = () => {
-  // Your contact form JSX here
+    <div className={styles.contactinfo}>
+      <div className={styles.contactitem}>
+        <FaEnvelope />
+        <p>niccolobelvedere01@gmail.com</p>
+      </div>
+      <div className={styles.contactitem}> 
+        <FaMapMarkerAlt />
+        <p>Milan, Italy</p>
+      </div>
+      <div className={styles.contactitem}>
+        <FaPhone />
+        <p>(+39) 3347227607</p>
+      </div>
+    </div>
+    <form className={styles.contactform}>
+      <input type="text" name="name" placeholder="Name" />
+      <input type="email" name="email" placeholder="Email" />
+      <textarea name="message" placeholder="Message" />
+      <button type="submit">
+        <FaPaperPlane />
+          Send Message
+      </button>
+    </form>
 };
 
 export default ContactSection;
