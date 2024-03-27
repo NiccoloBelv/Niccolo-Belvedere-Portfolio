838f3b2773e999ccc7db13c81820189bb6495eb6
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-27 19:19:17 +0100

    Create ContactSection.js

---
+++ pages/ContactSection.js
@@ -0,0 +1,9 @@
+// ContactSection.js
+import React from 'react';
+import { FaEnvelope, FaPhone, FaMapMarkerAlt, FaPaperPlane } from 'react-icons/fa';
+
+const ContactSection = () => {
+  // Your contact form JSX here
+};
+
+export default ContactSection;
