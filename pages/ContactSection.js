22e95fb4c34def79f21a0371542c45df6340c00f
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-27 19:28:19 +0100

    Update ContactSection.js

--- pages/ContactSection.js
+++ pages/ContactSection.js
@@ -1,31 +1,49 @@
-// ContactSection.js
 import React from 'react';
 import { FaEnvelope, FaPhone, FaMapMarkerAlt, FaPaperPlane } from 'react-icons/fa';
+import styles from '@styles/Home.module.css'; // Assicurati che il percorso sia corretto per il tuo CSS
 
 const ContactSection = () => {
-    <div className={styles.contactinfo}>
-      <div className={styles.contactitem}>
-        <FaEnvelope />
-        <p>niccolobelvedere01@gmail.com</p>
+  // Gestore dell'invio del form che previene il comportamento di default del form
+  // Se stai usando Netlify per il form, questa funzione può non essere necessaria
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Gestire l'invio del modulo qui se necessario
+  };
+
+  return (
+    <section id="contact" className={`${styles.contactSection}`}>
+      <h2>Contacts</h2>
+      <p>
+        My LinkedIn profile: <a href="https://www.linkedin.com/in/niccolo-belvedere/" target="_blank" rel="noopener noreferrer">Visit my LinkedIn</a><br />
+        Personal email: niccolobelvedere01@gmail.com<br />
+        Institutional email: niccolo.belvedere@studbocconi.it<br />
+        Mobile: (+39) 3347227607
+      </p>
+      <div className={styles.contactinfo}>
+        <div className={styles.contactitem}>
+          <FaEnvelope />
+          <p>niccolobelvedere01@gmail.com</p>
+        </div>
+        <div className={styles.contactitem}>
+          <FaMapMarkerAlt />
+          <p>Milan, Italy</p>
+        </div>
+        <div className={styles.contactitem}>
+          <FaPhone />
+          <p>(+39) 3347227607</p>
+        </div>
       </div>
-      <div className={styles.contactitem}> 
-        <FaMapMarkerAlt />
-        <p>Milan, Italy</p>
-      </div>
-      <div className={styles.contactitem}>
-        <FaPhone />
-        <p>(+39) 3347227607</p>
-      </div>
-    </div>
-    <form className={styles.contactform}>
-      <input type="text" name="name" placeholder="Name" />
-      <input type="email" name="email" placeholder="Email" />
-      <textarea name="message" placeholder="Message" />
-      <button type="submit">
-        <FaPaperPlane />
+      <form className={styles.contactForm} onSubmit={handleSubmit} netlify data-netlify="true">
+        <input type="text" name="name" placeholder="Name" required />
+        <input type="email" name="email" placeholder="Email" required />
+        <textarea name="message" placeholder="Message" required></textarea>
+        <button type="submit" className={styles.submitButton}>
+          <FaPaperPlane />
           Send Message
-      </button>
-    </form>
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default ContactSection;
