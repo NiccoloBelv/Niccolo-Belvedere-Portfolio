4baa71dfebf86edd8684e94eaebfe244a8f2faed
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-27 23:43:39 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -480,16 +480,16 @@ export default function Home() {
               Institutional email: niccolo.belvedere@studbocconi.it<br />
               Mobile: (+39) 3347227607
           </p>
-          <div className={styles.contactSection}>
-            <div className={styles.contactItem}>
+          <div className={styles.contactBar}>
+            <div className={styles.contactIcon}>
               <img src="email.png" alt="Email" />
               <p>niccolobelvedere01@gmail.com</p>
             </div>
-            <div className={styles.contactItem}>
+            <div className={styles.contactIcon}>
               <img src="position.png" alt="Address" />
               <p>Milan, Italy</p>
             </div>
-            <div className={styles.contactItem}>
+            <div className={styles.contactIcon}>
               <img src="phone.png" alt="Phone" />
               <p>(+39) 3347227607</p>
             </div>
