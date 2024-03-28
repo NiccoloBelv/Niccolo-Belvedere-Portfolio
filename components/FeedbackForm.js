e03a521f48c702682d7f700e598f54441ef7005c
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-28 18:33:30 +0100

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -1,38 +1,31 @@
-import React from 'react';
-import styles from '@styles/FeedbackForm.module.css'; // Update this path if necessary
+import styles from '@styles/FeedbackForm.module.css'
 
-const FeedbackForm = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Add form submission logic here
-  };
-
+export default function FeedbackForm() {
   return (
-    <div className={styles.feedbackForm}>
       <form
+        className={styles.form}
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
         name="feedback"
         method="POST"
         action="/success"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
       >
-        <p className={styles.hidden}>
-          <label>
-            Don’t fill this out if you’re human: <input name="bot-field" />
-          </label>
-        </p>
         <input type="hidden" name="form-name" value="feedback" />
+        <p className={styles.hidden}>
+            <label>
+            Don’t fill this out if you’re human: <input name="bot-field" />
+            </label>
+        </p>
+  
         <label htmlFor="name">Name</label>
-        <input className={styles.input} id="name" type="text" name="name" />
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+
         <label htmlFor="email">Email</label>
-        <input className={styles.input} id="email" type="email" name="email" required />
-        <label htmlFor="feedback">Leave a message</label>
-        <textarea className={styles.textarea} id="feedback" wrap="soft" name="feedback" required></textarea>
+        <input id="email" className={styles['form-field']} type="email" name="email" required />
+
+        <label htmlFor="feedback">What is your feedback?</label>
+        <textarea id="feedback" className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
         <button className={styles.button} type="submit">Submit</button>
       </form>
-    </div>
-  );
-};
-
-export default FeedbackForm;
+  )
+}
