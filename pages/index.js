d279777c99d4d30fe8b306ab4ea8d340ffc98e94
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-28 18:55:05 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 import FeedbackForm from '@components/FeedbackForm';
