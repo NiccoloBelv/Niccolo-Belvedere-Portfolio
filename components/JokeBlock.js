d1aac5f2797e88aeb818d979a813725d724a611f
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-28 16:22:23 +0100

    Update JokeBlock.js

--- components/JokeBlock.js
+++ components/JokeBlock.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '@styles/JokeBlock.css'; // Assicurati che il file CSS sia presente e correttamente importato
+import '@styles/JokeBlock.module.css'; // Assicurati che il file CSS sia presente e correttamente importato
 
 const JokeBlock = () => {
   const [joke, setJoke] = useState('');
