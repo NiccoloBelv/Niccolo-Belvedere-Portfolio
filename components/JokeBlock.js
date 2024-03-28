d25785328b5ec52d05ee5b425f27957b3e14e779
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-28 18:33:54 +0100

    Update JokeBlock.js

--- components/JokeBlock.js
+++ components/JokeBlock.js
@@ -1,26 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import styles from '@styles/JokeBlock.module.css'; // Update this path if necessary
+import { useEffect, useState } from "react";
+import styles from './JokeBlock.module.css';
 
-const JokeBlock = () => {
-  const [joke, setJoke] = useState('');
+export default function JokeBlock() {
+    const [joke, setJoke] = useState("")
 
-  useEffect(() => {
-    const fetchJoke = async () => {
-      const response = await fetch('/api/joke');
-      const data = await response.json();
-      setJoke(data);
-    };
+    useEffect(() => {
+        fetch('/api/joke')
+        .then(res => res.json())
+        .then(jokeJSON=> {
+            setJoke(jokeJSON)
+        })
+    }, []);
 
-    fetchJoke();
-  }, []);
-
-  return (
-    <article className={styles.jokeBlock}>
-      <blockquote className={styles.blockquote}>
-        <p>{joke}</p>
-      </blockquote>
-    </article>
-  );
-};
-
-export default JokeBlock;
+    return (
+        <blockquote className={styles.quote}>{joke}</blockquote>
+    )
+}
