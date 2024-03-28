d347c3c4e4e24996fca5bf2c819d505fe0314d65
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-28 16:10:20 +0100

    Create ContactForm.js

---
+++ components/ContactForm.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import './JokeBlock.css'; // Assicurati che il file CSS sia presente e correttamente importato
+
+const JokeBlock = () => {
+  const [joke, setJoke] = useState('');
+
+  useEffect(() => {
+    const fetchJoke = async () => {
+      const response = await fetch('/api/joke');
+      const data = await response.json();
+      setJoke(data);
+    };
+
+    fetchJoke();
+  }, []); // La lista vuota di dipendenze [] assicura che l'effetto venga eseguito solo al montaggio
+
+  return (
+    <article>
+      <blockquote>
+        <p>{joke}</p>
+      </blockquote>
+    </article>
+  );
+};
+
+export default JokeBlock;
