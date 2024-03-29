8eba1ea5a2b68dfd185195e4550a2b85da224b72
NiccoloBelv <161612404+NiccoloBelv@users.noreply.github.com>
2024-03-29 11:47:50 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8888/',
+    baseUrl: 'http://localhost:8888',
     supportFile: false,
   },
 });
